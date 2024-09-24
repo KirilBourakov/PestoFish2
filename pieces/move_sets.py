c32c879b15615652ce80ba37cbad46d7fd18b677
Kiril B <kirbou06012@gmail.com>
2024-09-24 19:16:32 -0300

    created generic movsets

---
+++ pieces/move_sets.py
@@ -0,0 +1,31 @@
+pawn_moves = [(0, 1)]
+
+king_moves = [
+    (-1, 1), (0, 1), (1,1),
+    (-1,0), (1,0),
+    (-1,-1), (0,-1), (1,-1)
+]
+
+knight_moves = [
+    (+3, -1), (+3, +1),
+    (-3, -1), (-3, +1),
+    (-1, +3), (+1, +3),
+    (-1, -3), (+1, -3),
+]
+
+rook_moves = []
+for i in range(1, 16):
+    rook_moves.append((i-8, 0))
+    rook_moves.append((0, i-8))
+rook_moves.remove((0,0))
+
+bishop_moves = []
+for i in range(1, 16):
+    bishop_moves.append((i-8, i-8))
+    bishop_moves.append((-(i-8), i-8))
+bishop_moves.remove((0,0))
+bishop_moves.remove((0,0))
+
+queen_moves = rook_moves.copy()
+queen_moves.extend(bishop_moves)
+print(queen_moves)
