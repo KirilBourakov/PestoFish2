fa4e195cbeeffc8bad9bb603360413325f87820c
Ege_Sona <egesona@hotmail.com>
2024-09-24 18:35:55 -0300

    black pieces added to pieces.py

---
+++ pieces/black_pieces.py
@@ -0,0 +1,9 @@
+from pieces.abstract_piece import Abstract_Piece
+import assets.assets as assets
+
+rook = Abstract_Piece(assets.b_rook, [])
+bishop = Abstract_Piece(assets.b_bishop, [])
+knight = Abstract_Piece(assets.b_knight, [])
+king = Abstract_Piece(assets.b_king, [])
+queen = Abstract_Piece(assets.b_queen, [])
+pawn = Abstract_Piece(assets.b_pawn, [])
