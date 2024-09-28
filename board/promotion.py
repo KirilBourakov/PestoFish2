4ba743ce603372429db4d511cd6b68c5c81c191d
Kiril B <kirbou06012@gmail.com>
2024-09-28 10:31:12 -0300

    added box around which promotion option you have selected

--- board/promotion.py
+++ board/promotion.py
@@ -9,6 +9,12 @@ class Promotion():
         self.pos = pos
         self.assets = [wp.queen, wp.rookL, wp.knight, wp.bishop] if pos[1] == 0 else [bp.queen, bp.rookL, bp.knight, bp.bishop]
         self.direction = 1 if pos[1] == 0 else -1
+        self.y_positions = [
+            self.pos[1],
+            self.pos[1] + 1 * self.direction,
+            self.pos[1] + 2 * self.direction,
+            self.pos[1] + 3 * self.direction
+        ]
 
     def show(self):
         gridx, gridy = self.pos
@@ -17,3 +23,12 @@ class Promotion():
             window.blit(assets.promotion_circle, (gridx*globals.grid_size,gridy*globals.grid_size))
             asset.show(gridx, gridy)
             gridy += self.direction
+
+        mousepos = pygame.mouse.get_pos()
+        mousex, mousey = int(mousepos[0]/globals.grid_size), int(mousepos[1]/globals.grid_size)
+        if mousex == gridx and mousey in self.y_positions:
+            pygame.draw.rect(
+                window, (255, 0, 0), 
+                pygame.Rect(mousex*globals.grid_size, mousey*globals.grid_size, globals.grid_size, globals.grid_size),
+                width=1
+            )
