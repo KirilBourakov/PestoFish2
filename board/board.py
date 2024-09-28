3f76b211d0c68f33846f46999992ec0569f6ba80
Kiril B <kirbou06012@gmail.com>
2024-09-28 11:20:24 -0300

    moved get_legal_moves to abstract piece

--- board/board.py
+++ board/board.py
@@ -57,57 +57,9 @@ class Chess_Board():
             return globals.PIECE_WHITE
         return globals.PIECE_BLACK
         
-    def get_legal_moves(self):
-        piece = self.board[self.selected_square[1]][self.selected_square[0]]
-        moves = piece.getPossibleMoves(self.selected_square)
-        oldx, oldy = self.selected_square
-
-        king_pos = self.get_king_pos(self.get_turn())    
-
-        # given all possibly legal moves, loop over and remove illegal ones
-        purged_moves = []
-        for move in moves:
-            legal = True
-
-            # certain moves come with functional conditions. If the move does so, make sure the conditions are met
-            if (len(move) == 3):
-                newx, newy, func = move
-                possible = func(old_position=self.selected_square, color=piece.color, new_position=(newx, newy), board=self.board, move_num=self.move_counter)
-                if (not possible[0]):
-                    continue
-                move = (newx, newy, possible[1])
-            else:
-                newx, newy = move
-            
-            x_walker, y_walker = oldx, oldy
-            # This checks that there is nothing blocking you from moving to that square
-            while (abs(x_walker-newx) > 1 or abs(y_walker-newy) > 1) and piece.hops == False:
-                # walk across, taking the same path as the peice.  
-                if x_walker < newx:
-                    x_walker += 1
-                if x_walker > newx:
-                    x_walker -= 1
-                if y_walker < newy:
-                    y_walker += 1
-                if y_walker > newy:
-                    y_walker -= 1
-                # if you meet another peice, the move is illegal
-                board_square_not_empty = self.board[y_walker][x_walker] is not None
-                if board_square_not_empty:
-                    not_occupied_by_en_passent = self.board[y_walker][x_walker].type != globals.EN_PASSENT_FLAG
-                    if not_occupied_by_en_passent:
-                        legal = False
-        
-            if (self.board[newy][newx] is not None) and (piece.color == self.board[newy][newx].color) and (self.board[newy][newx].type != globals.EN_PASSENT_FLAG):
-                continue
-            
-            if legal: 
-                purged_moves.append(move)
-                       
-        return purged_moves
 
     def make_legal_move(self, newx, newy): 
-        moves = self.get_legal_moves()
+        moves = self.board[self.selected_square[1]][self.selected_square[0]].get_legal_moves(self)
         piece_location = (self.selected_square[0], self.selected_square[1])
         if (newx, newy) in moves or (newx, newy, globals.NORMAL_FLAG) in moves:
             self.move(piece_location, (newx, newy))
--- pieces/abstract_piece.py
+++ pieces/abstract_piece.py
@@ -20,6 +20,55 @@ class Abstract_Piece():
                 else:
                     moves_list.append(new_pos,)
         return moves_list
+    
+    def get_legal_moves(self, board_obj):
+        piece = board_obj.board[board_obj.selected_square[1]][board_obj.selected_square[0]]
+        moves = piece.getPossibleMoves(board_obj.selected_square)
+        oldx, oldy = board_obj.selected_square
+
+        king_pos = board_obj.get_king_pos(board_obj.get_turn())    
+
+        # given all possibly legal moves, loop over and remove illegal ones
+        purged_moves = []
+        for move in moves:
+            legal = True
+
+            # certain moves come with functional conditions. If the move does so, make sure the conditions are met
+            if (len(move) == 3):
+                newx, newy, func = move
+                possible = func(old_position=board_obj.selected_square, color=piece.color, new_position=(newx, newy), board=board_obj.board, move_num=board_obj.move_counter)
+                if (not possible[0]):
+                    continue
+                move = (newx, newy, possible[1])
+            else:
+                newx, newy = move
+            
+            x_walker, y_walker = oldx, oldy
+            # This checks that there is nothing blocking you from moving to that square
+            while (abs(x_walker-newx) > 1 or abs(y_walker-newy) > 1) and piece.hops == False:
+                # walk across, taking the same path as the peice.  
+                if x_walker < newx:
+                    x_walker += 1
+                if x_walker > newx:
+                    x_walker -= 1
+                if y_walker < newy:
+                    y_walker += 1
+                if y_walker > newy:
+                    y_walker -= 1
+                # if you meet another peice, the move is illegal
+                board_square_not_empty = board_obj.board[y_walker][x_walker] is not None
+                if board_square_not_empty:
+                    not_occupied_by_en_passent = board_obj.board[y_walker][x_walker].type != globals.EN_PASSENT_FLAG
+                    if not_occupied_by_en_passent:
+                        legal = False
+        
+            if (board_obj.board[newy][newx] is not None) and (piece.color == board_obj.board[newy][newx].color) and (board_obj.board[newy][newx].type != globals.EN_PASSENT_FLAG):
+                continue
+            
+            if legal: 
+                purged_moves.append(move)
+                       
+        return purged_moves
 
     def show(self, x, y):
         window = pygame.display.get_surface()
