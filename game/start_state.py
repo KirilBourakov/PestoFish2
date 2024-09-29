a1df5bbd6ab619e65aed5f18c91365bee899a2fb
Kiril B <kirbou06012@gmail.com>
2024-09-28 22:15:06 -0300

    updated graphics on start_state

--- game/start_state.py
+++ game/start_state.py
@@ -39,12 +39,25 @@ class Start_State(Abstract_State):
         window.blit(render, (int((globals.appsize-x)/2), int((globals.appsize-y)/6)))
 
         offset = (y+50)
-        for option in self.options:
+        for i, option in enumerate(self.options):
             render = assets.text_large.render(option, False, "white")
             horizontal_size, virtical_size= assets.text_large.size(option)
 
-            window.blit(render, (int((globals.appsize-horizontal_size)/2), int((globals.appsize-virtical_size)/6)+offset))
+            xpos, ypos = int((globals.appsize-horizontal_size)/2), int((globals.appsize-virtical_size)/6)+offset
+            window.blit(render, (xpos, ypos))
             offset += virtical_size
+
+            # draw red bars to indicate selection
+            if i == self.chosen:
+                pygame.draw.rect(
+                    window, (255, 0, 0), 
+                    pygame.Rect(xpos-120, ypos+20, 80, virtical_size-30),
+                )
+                pygame.draw.rect(
+                    window, (255, 0, 0), 
+                    pygame.Rect(xpos+horizontal_size+40, ypos+20, 80, virtical_size-30),
+                )
+
         offset += virtical_size
 
         render = assets.text_small.render("Hit <Enter> to Start", False, "white")
