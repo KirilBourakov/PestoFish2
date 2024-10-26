80442c43bcc30a3d3e88ab4cdad7f06cb10eca3c
Kiril B <kirbou06012@gmail.com>
2024-10-26 13:12:06 -0300

    created first two classes

---
+++ engine/src/engine.py
@@ -0,0 +1,6 @@
+
+
+class engine():
+    def __init__(self):
+        generator = None #should contain a class that is used to generate moves
+        evaluator = None #should contain a class that evaluates a given move position
---
+++ engine/src/generator.py
@@ -0,0 +1,3 @@
+class generator():
+    def get_moves(self, board: list[str]) -> list[tuple[int, int]]:
+        '''given a board state return a list of possible moves'''
