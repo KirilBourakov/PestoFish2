8e7cbfe1124d587b3764dd027bb73cbbcd562240
Kiril B <kirbou06012@gmail.com>
2024-11-05 18:00:42 -0400

    added tests

--- engine/tests/generator_tests.py
+++ engine/tests/generator_tests.py
@@ -16,3 +16,28 @@ class GeneratorTests(unittest.TestCase):
         
         s = generator.is_legal_move(board1, (4,7), (5,6), (5,4))
         self.assertFalse(s)
+
+
+        board2 = [['Br', '  ', 'bb', 'bq', 'BK', 'bb', 'bk', 'Br'],
+                ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+                ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+                ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+                ['wp', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+                ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+                ['  ', 'wp', 'bk', 'wp', 'wp', 'wp', 'wp', 'wp'],
+                ['Wr', 'wk', 'wb', 'wq', 'WK', 'wb', 'wk', 'Wr']]
+        s = generator.is_legal_move(board2, (4,7), (0,4), (0,3))
+        self.assertFalse(s)
+
+    def test_get_moves(self):
+        generator = Generator()
+        board1 = [['Br', '  ', 'bb', 'bq', 'BK', 'bb', 'bk', 'Br'],
+                ['bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp', 'bp'],
+                ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+                ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+                ['wp', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+                ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+                ['  ', 'wp', 'bk', 'wp', 'wp', 'wp', 'wp', 'wp'],
+                ['Wr', 'wk', 'wb', 'wq', 'WK', 'wb', 'wk', 'Wr']]
+        s = generator.get_moves(board1, (4,7))
+        self.assertEqual(len(s), 1)
