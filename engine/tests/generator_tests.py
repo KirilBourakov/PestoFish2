58f7bf33c0658b2145053bce439e13fc23392778
Kiril B <kirbou06012@gmail.com>
2024-11-05 14:05:58 -0400

    added generator_tests

--- engine/tests/generator_tests.py
+++ engine/tests/generator_tests.py
@@ -3,6 +3,16 @@ import unittest
 from engine.src.generator.generator import Generator
 
 class GeneratorTests(unittest.TestCase):
-    def test_get_moves(self):
+    def test_is_legal_move(self):
         generator = Generator()
-        pass
+        board1 = [['Br', 'bk', 'bb', '  ', 'BK', 'bb', 'bk', 'Br'],
+                ['bp', 'bp', 'bp', 'bp', '  ', 'bp', 'bp', 'bp'],
+                ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+                ['  ', '  ', '  ', '  ', 'bp', '  ', '  ', '  '],
+                ['wp', '  ', '  ', '  ', '  ', '  ', '  ', 'bq'],
+                ['  ', '  ', '  ', '  ', '  ', '  ', '  ', '  '],
+                ['  ', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp', 'wp'],
+                ['Wr', 'wk', 'wb', 'wq', 'WK', 'wb', 'wk', 'Wr']]
+        
+        s = generator.is_legal_move(board1, (4,7), (5,6), (5,4))
+        self.assertFalse(s)
--- runtests.py
+++ runtests.py
@@ -1,9 +1,11 @@
 import unittest
 from engine.tests.moves_tests import MovesTests
+from engine.tests.generator_tests import GeneratorTests
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(MovesTests))
+    suite.addTest(unittest.makeSuite(GeneratorTests))
     return suite
 
 if __name__ == '__main__':
