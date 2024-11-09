adff4ea68df94437f1b86d2a7a176c38cb35822a
Kiril B <kirbou06012@gmail.com>
2024-11-08 21:18:43 -0400

    added test

--- runtests.py
+++ runtests.py
@@ -2,12 +2,14 @@ import unittest
 from engine.tests.moves_tests import MovesTests
 from engine.tests.generator_tests import GeneratorTests
 from engine.tests.engine_result_tests import EngineResultTests
+from engine.tests.engine_value_tests import EngineValueTests
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(MovesTests))
     suite.addTest(unittest.makeSuite(GeneratorTests))
     suite.addTest(unittest.makeSuite(EngineResultTests))
+    suite.addTest(unittest.makeSuite(EngineValueTests))
     return suite
 
 if __name__ == '__main__':
