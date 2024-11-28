19c13b488030e4ad80886b73e7a44d29af96347f
Kiril Bourakov <kirbou06012@gmail.com>
2024-11-27 23:21:49 -0400

    finished parser

--- builders/open-db/parser.py
+++ builders/open-db/parser.py
@@ -3,10 +3,11 @@ import sys
 import re
 
 class Parse(HTMLParser):
-    def __init__(self):
+    def __init__(self, out):
         super().__init__()
         self.inli = False
-        self.searcher = re.compile(r"/*1. (N|)[a-h][0-9] (N|)[a-h][0-9]*") #
+        self.searcher = re.compile(r"/*1. (N|)[a-h][0-9] (N|)[a-h][0-9]*")
+        self.file = open(out, 'w')
 
     def handle_starttag(self, tag, attrs):
         if tag == "li":
@@ -22,15 +23,22 @@ class Parse(HTMLParser):
         if not data.strip():
             return
         data = data.strip().replace("\n", " ")
-        if self.searcher.search(data):
-            print(data)
+        result = self.searcher.search(data)
+        if result:
+            self.file.write(data[result.start():])
+            self.file.write(" 1-0")
+            self.file.write('\n')
 
+    def finish(self):
+        self.file.close()
 
 def main():
-    parser = Parse()
+    out = sys.argv[2]
+    parser = Parse(out)
     with open(sys.argv[1], encoding="cp437") as file:
         for row in file:
             parser.feed(row)
+    parser.finish()
 
 if __name__ == "__main__":
     main()
