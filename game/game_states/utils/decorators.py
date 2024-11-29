8ce0a1bfa36b9fb4975b30795670e881cff12ad0
Kiril Bourakov <kirbou06012@gmail.com>
2024-11-29 18:21:39 -0400

    engine thinking no longer blocks moves

--- game/game_states/utils/decorators.py
+++ game/game_states/utils/decorators.py
@@ -1,5 +1,7 @@
 import game.constants.globals as globals
+from threading import Thread
 from .EngineAPI import EngineAPI
+import pygame
 
 def disable_on_engine_turn(func):
     def wrapper(self, *args, **kwargs):
@@ -13,9 +15,10 @@ def disable_on_engine_turn(func):
 def run_engine(func):
     def wrapper(self, *args, **kwargs):
         func(self, *args, **kwargs)
+        pygame.display.update()
         black_engine_turn = self.game_type == globals.GAME_TYPE_ENGINE_BLACK and self.move_counter % 2 != 0 and self.promotion == None
         white_engine_turn = self.game_type == globals.GAME_TYPE_ENGINE_WHITE and self.move_counter % 2 == 0 and self.promotion == None
         if black_engine_turn or white_engine_turn:
-            EngineAPI.engine_make_move(self)
+            EngineAPI.engine_make_move(self)    
         return  
     return wrapper
