47413b817b9e35b9aa14bbc5dd22717d8ee9c24f
Kiril Bourakov <kirbou06012@gmail.com>
2024-11-29 19:54:32 -0400

    changed == to is

--- game/game_states/utils/decorators.py
+++ game/game_states/utils/decorators.py
@@ -16,8 +16,8 @@ def run_engine(func):
     def wrapper(self, *args, **kwargs):
         func(self, *args, **kwargs)
         pygame.display.update()
-        black_engine_turn = self.game_type == globals.GAME_TYPE_ENGINE_BLACK and self.move_counter % 2 != 0 and self.promotion == None
-        white_engine_turn = self.game_type == globals.GAME_TYPE_ENGINE_WHITE and self.move_counter % 2 == 0 and self.promotion == None
+        black_engine_turn = self.game_type == globals.GAME_TYPE_ENGINE_BLACK and self.move_counter % 2 != 0 and self.promotion is None
+        white_engine_turn = self.game_type == globals.GAME_TYPE_ENGINE_WHITE and self.move_counter % 2 == 0 and self.promotion is None
         if black_engine_turn or white_engine_turn:
             EngineAPI.engine_make_move(self)    
         return  
