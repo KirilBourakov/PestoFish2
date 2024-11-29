48bbdc3ab3ac4c0bee6f2e4f393b70404282cdea
Kiril Bourakov <kirbou06012@gmail.com>
2024-11-29 09:47:54 -0400

    added long castle test

--- engine/tests/searcher_tests.py
+++ engine/tests/searcher_tests.py
@@ -12,6 +12,12 @@ class SearcherTests(unittest.TestCase):
         self.assertEqual(move['original'], (4,7))
         self.assertEqual(move['new'], (6,7))
 
+        # black long castle
+        move = searcher.get_move_from_fen('r3kbnr/p1p1qppp/b1np4/4p3/P1B1P3/5N2/2PP1PPP/RNBQ1RK1',
+                                          '2kr1bnr/p1p1qppp/b1np4/4p3/P1B1P3/5N2/2PP1PPP/RNBQ1RK1')
+        self.assertEqual(move['original'], (4,0))
+        self.assertEqual(move['new'], (2,0))
+
         # black enpassent
         move = searcher.get_move_from_fen('rnb1kbnr/p1p1qppp/3p4/4p3/Pp2P3/3B1N2/1PPP1PPP/RNBQ1RK1',
                                           'rnb1kbnr/p1p1qppp/3p4/4p3/4P3/p2B1N2/1PPP1PPP/RNBQ1RK1')
