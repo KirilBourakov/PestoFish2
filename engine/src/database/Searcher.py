cd301a990ccb598013a27ae4f2f81036ada9c546
Kiril Bourakov <kirbou06012@gmail.com>
2024-12-07 12:43:42 -0400

    updated searcher to having static variables

--- engine/src/database/Searcher.py
+++ engine/src/database/Searcher.py
@@ -5,29 +5,29 @@ from engine.src.constants.engineTypes import boardType, MoveType
 from engine.src.constants.static import EMPTY, WHITE, BLACK, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING, EN_PASSENT
 from engine.src.helpers.square_analysis import is_empty, get_type
 
-db_path = os.path.join(os.path.dirname(__file__), 'open.db')
-db = sqlite3.connect(db_path)
-executor = db.cursor()
+
 class Searcher():
-    def __init__(self) -> None:
+    db_path = os.path.join(os.path.dirname(__file__), 'open.db')
+    db = sqlite3.connect(db_path)
+    executor = db.cursor()
 
-        self.map = {
-            f'{WHITE}{PAWN}': 'P',
-            f'{WHITE}{KNIGHT}': 'N',
-            f'{WHITE}{BISHOP}': 'B',
-            f'{WHITE}{ROOK}': 'R',
-            f'{WHITE}{QUEEN}': 'Q',
-            f'{WHITE}{KING}': 'K',
+    map = {
+        f'{WHITE}{PAWN}': 'P',
+        f'{WHITE}{KNIGHT}': 'N',
+        f'{WHITE}{BISHOP}': 'B',
+        f'{WHITE}{ROOK}': 'R',
+        f'{WHITE}{QUEEN}': 'Q',
+        f'{WHITE}{KING}': 'K',
 
-            f'{BLACK}{PAWN}': 'p',
-            f'{BLACK}{KNIGHT}': 'n',
-            f'{BLACK}{BISHOP}': 'b',
-            f'{BLACK}{ROOK}': 'r',
-            f'{BLACK}{QUEEN}': 'q',
-            f'{BLACK}{KING}': 'k',
-        }
-
-        self.square_letter_map = {
+        f'{BLACK}{PAWN}': 'p',
+        f'{BLACK}{KNIGHT}': 'n',
+        f'{BLACK}{BISHOP}': 'b',
+        f'{BLACK}{ROOK}': 'r',
+        f'{BLACK}{QUEEN}': 'q',
+        f'{BLACK}{KING}': 'k',
+    }
+    
+    square_letter_map = {
             0: 'a',
             1: 'b',
             2: 'c',
@@ -41,13 +41,13 @@ class Searcher():
     def query_theory(self, board: boardType, color_to_move: str) -> MoveType:
         '''Given a board state, return a random theoretical move it has.'''
         fen = self.board_to_fen(board, color_to_move)
-        query_results = executor.execute('SELECT new from transpositions WHERE inital == (SELECT pk FROM positions WHERE position == ?)', (fen,))
+        query_results = self.executor.execute('SELECT new from transpositions WHERE inital == (SELECT pk FROM positions WHERE position == ?)', (fen,))
         results: list[str] = query_results.fetchall()
         if len(results) == 0:
             return MoveType(original=(-1,-1), new=(-1,-1), rating=0, promotion='')
         chosen = random.choice(results)
 
-        new_pos = executor.execute('SELECT position FROM positions WHERE pk == ?', (chosen[0],))
+        new_pos = self.executor.execute('SELECT position FROM positions WHERE pk == ?', (chosen[0],))
         new_pos_str: str = new_pos.fetchone()[0]
 
         return self.get_move_from_fen(fen.split()[0], new_pos_str.split()[0])
