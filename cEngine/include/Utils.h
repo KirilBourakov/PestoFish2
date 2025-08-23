ad366661e8a38d91969a884cffd7c629c4ac3e23
Kiril B <kirbou06012@gmail.com>
2025-08-23 20:53:55 -0300

    restructured methods

--- cEngine/include/Utils.h
+++ cEngine/include/Utils.h
@@ -6,31 +6,7 @@
 #define UTILS_H
 #include "Types.h"
 
-bool castleAllowed(Color color, CastleType type, int castleRights) {
-    int bit;
-    if (type == SHORT && color == WHITE) {
-        bit = 3;
-    } else if (type == LONG && color == WHITE) {
-        bit = 2;
-    } else if (type == SHORT && color == BLACK) {
-        bit = 1;
-    } else {
-        bit = 0;
-    }
-    return (castleRights >> bit) & 1;
-}
-void disAllowCastle(Color color, CastleType type, int& castleRights) {
-    int bit;
-    if (type == SHORT && color == WHITE) {
-        bit = 3;
-    } else if (type == LONG && color == WHITE) {
-        bit = 2;
-    } else if (type == SHORT && color == BLACK) {
-        bit = 1;
-    } else {
-        bit = 0;
-    }
-    castleRights &= ~(1 << bit);;
-}
+bool castleAllowed(Color color, CastleType type, int castleRights);
+void disAllowCastle(Color color, CastleType type, int& castleRights);
 
 #endif //UTILS_H
--- cEngine/src/Utils.cpp
+++ cEngine/src/Utils.cpp
@@ -4,6 +4,29 @@
 
 #include "Utils.h"
 
-bool castleAllowed(Color color, CastleType type, int castleRights) {
-
+bool castleAllowed(const Color color, const CastleType type, const int castleRights) {
+    int bit;
+    if (type == SHORT && color == WHITE) {
+        bit = 3;
+    } else if (type == LONG && color == WHITE) {
+        bit = 2;
+    } else if (type == SHORT && color == BLACK) {
+        bit = 1;
+    } else {
+        bit = 0;
+    }
+    return (castleRights >> bit) & 1;
+}
+void disAllowCastle(const Color color, const CastleType type, int& castleRights) {
+    int bit;
+    if (type == SHORT && color == WHITE) {
+        bit = 3;
+    } else if (type == LONG && color == WHITE) {
+        bit = 2;
+    } else if (type == SHORT && color == BLACK) {
+        bit = 1;
+    } else {
+        bit = 0;
+    }
+    castleRights &= ~(1 << bit);;
 }
