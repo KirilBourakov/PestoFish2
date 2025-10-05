d6b17c32863d7b389ae3f27c36e7180996d0750a
Kiril B <kirbou06012@gmail.com>
2025-10-04 21:41:04 -0300

    removed python engine

--- engine/src/evaluator/heuristics/constants.py
+++
@@ -1,7 +0,0 @@
-from engine.src.constants.static import PAWN, ROOK, BISHOP, KNIGHT, QUEEN, WHITE, KING, EMPTY, MIDDLE_GAME, END_GAME
-import json
-import os
-
-piece_value_map: dict[str, dict[str, list[list[int]]]] = json.load(open(
-    os.path.join(os.path.dirname(__file__), 'db.json')
-))
--- engine/src/helpers/helpers.py
+++
@@ -1,5 +0,0 @@
-from engine.src.constants.static import BLACK, WHITE
-
-def flip(color: str) -> str:
-    '''given a color, return the opposite one'''
-    return WHITE if color == BLACK else BLACK
