5670858f52dc6084ba9f0ee214730ae74b861c17
Kiril <kirbou06012@gmail.com>
2025-11-20 17:01:07 -0800

    renamed Perft

--- cEngine/test/TestPerft.cpp
+++ cEngine/test/TestPerft.cpp
@@ -90,36 +90,31 @@ TEST(Perft, undoInMiddleGame) {
 // TEST(Preft, depth4) {
 //     ASSERT_EQ(Perft(State{}, 4), 197281);
 // }
-TEST(Preft, startpos5) {
+TEST(Perft, startpos5) {
     State state;
     ASSERT_EQ(Perft(state, 5), 4865609);
 }
-TEST(Preft, Kiwipete4) {
+TEST(Perft, Kiwipete4) {
     State state = fenToState("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
     ASSERT_EQ(Perft(state, 4), 4085603);
 }
-TEST(Preft, EndGame4) {
+TEST(Perft, EndGame4) {
     State state = fenToState("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1 ");
     ASSERT_EQ(Perft(state, 4), 43238);
 }
-TEST(Preft, MiddleGameWhite4) {
+TEST(Perft, MiddleGameWhite4) {
     State state = fenToState("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1");
     ASSERT_EQ(Perft(state, 4), 422333);
 }
-TEST(Preft, MiddleGameBlack4) {
+TEST(Perft, MiddleGameBlack4) {
     State state = fenToState("r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1");
     ASSERT_EQ(Perft(state, 4), 422333);
 }
-TEST(Preft, TalkChessPos) {
+TEST(Perft, TalkChessPos) {
     State state = fenToState("rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8");
     ASSERT_EQ(Perft(state, 3),  62379);
 }
-TEST(Preft, AltPreft4) {
+TEST(Perft, AltPerft4) {
     State state = fenToState("r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10");
     ASSERT_EQ(Perft(state, 4),  3894594);
 }
-
-
-// TEST(Preft, depth6) {
-//     ASSERT_EQ(Perft(State{}, 6), 119060324);
-// }
