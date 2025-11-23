51b9bc6fd73270388732c61c0d2ad83e5f145d0d
Kiril <kirbou06012@gmail.com>
2025-11-23 15:54:15 -0400

    added BitBoardManager

--- cEngine/include/NewBoard.hpp
+++ cEngine/include/NewBoard.hpp
@@ -4,6 +4,7 @@
 
 #pragma once
 #include <array>
+#include <cassert>
 #include <format>
 #include <vector>
 
@@ -11,13 +12,79 @@
 #include "ModuleOnly/Enums.hpp"
 #include "ModuleOnly/Move.hpp"
 
-
-
 constexpr int BOARD_SIZE = 8;
+
+class BitBoardManager {
+public:
+    [[nodiscard]] uint64_t at(const Pieces::Piece piece) const {
+        assert(piece != Pieces::EMPTY && "EMPTY has no bitboard");
+        return board[indexOf(piece)];
+    }
+
+    [[nodiscard]] uint64_t at(const Color color) const {
+        return colorBoard[static_cast<size_t>(color)]; // WHITE = 0, BLACK = 1
+    }
+
+    void insert(const Pieces::Piece piece, const int y, const int x) {
+        assert(piece != Pieces::EMPTY && "EMPTY has no bitboard");
+
+        const uint64_t mask = (1ULL << shiftValue(y, x));
+        at_mut(piece) |= mask;
+        at_mut(Pieces::piece_color(piece)) |= mask;
+    }
+
+    static int shiftValue(const int y, const int x) {
+        return y * BOARD_SIZE + x;
+    }
+
+private:
+    std::array<uint64_t, 12> board{};
+    std::array<uint64_t, 2> colorBoard{};
+
+    uint64_t& at_mut(const Color color) {
+        return colorBoard[static_cast<size_t>(color)];
+    }
+
+    uint64_t& at_mut(const Pieces::Piece piece) {
+        return board[indexOf(piece)];
+    }
+
+    static size_t indexOf(const Pieces::Piece piece) {
+        switch (piece) {
+            case Pieces::WHITE_PAWN:   return 0;
+            case Pieces::WHITE_KNIGHT: return 1;
+            case Pieces::WHITE_BISHOP: return 2;
+            case Pieces::WHITE_ROOK:   return 3;
+            case Pieces::WHITE_QUEEN:  return 4;
+            case Pieces::WHITE_KING:   return 5;
+
+            case Pieces::BLACK_PAWN:   return 6;
+            case Pieces::BLACK_KNIGHT: return 7;
+            case Pieces::BLACK_BISHOP: return 8;
+            case Pieces::BLACK_ROOK:   return 9;
+            case Pieces::BLACK_QUEEN:  return 10;
+            case Pieces::BLACK_KING:   return 11;
+
+            case Pieces::EMPTY:
+            default:
+                throw std::invalid_argument("EMPTY has no bitboard index");
+        }
+    }
+};
+
 class NewBoard {
 public:
-    explicit NewBoard(std::array<std::array<Pieces::Piece, BOARD_SIZE>, BOARD_SIZE> inp)
-        : board(inp) {}
+    explicit NewBoard(const std::array<std::array<Pieces::Piece, BOARD_SIZE>, BOARD_SIZE> &inp)
+        : board(inp)
+    {
+        for (int y = 0; y < BOARD_SIZE; y++) {
+            for (int x = 0; x < BOARD_SIZE; x++) {
+                if (board[y][x] != Pieces::EMPTY) {
+                    bitBoards.insert(board[y][x], y, x);
+                }
+            }
+        }
+    }
 
     [[nodiscard]] std::vector<Move> getPseudoLegal(const Color activeColor, const std::optional<BoardPosition> enPassantSquare,
                                                    int castlingRights) const {
@@ -76,6 +143,7 @@ public:
 
 private:
     std::array<std::array<Pieces::Piece, BOARD_SIZE>, BOARD_SIZE> board;
+    BitBoardManager bitBoards{};
 };
 
 /**
