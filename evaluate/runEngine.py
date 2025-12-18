bb127b07c5e4164c6565e49f96853cd7e08808c2
Kiril <kirbou06012@gmail.com>
2025-12-17 20:57:20 -0400

    added running of engine

--- evaluate/runEngine.py
+++ evaluate/runEngine.py
@@ -1,49 +1,110 @@
 import logging
+import os
 from collections import defaultdict
+from pathlib import Path
+from plistlib import InvalidFileException
+from typing import Sequence
 
 import chess
 import chess.engine
+from chess.engine import SimpleEngine
+from sqlalchemy import create_engine, select, exists
+from sqlalchemy.orm import Session
 from tqdm import tqdm
+
+from Models import Engine, Tournament, ChessPosition, Match
 
 ENGINE = r"E:\Programming\Projects\PestoFish2\evaluate\bitboards.exe"
 OLD_ENGINE = r"E:\Programming\Projects\PestoFish2\evaluate\normal.exe"
 
 
-def play_game(fen):
-    engine1 = chess.engine.SimpleEngine.popen_uci(ENGINE)
-    engine2 = chess.engine.SimpleEngine.popen_uci(OLD_ENGINE)
+def play_game(fen: str, white: str, black: str) -> str:
+    white_engine = chess.engine.SimpleEngine.popen_uci(white)
+    black_engine = chess.engine.SimpleEngine.popen_uci(black)
+
     board = chess.Board(fen=fen)
 
     while not board.is_game_over():
         if board.turn == chess.WHITE:
-            result = engine2.play(board, chess.engine.Limit(time=0.1))
+            result = white_engine.play(board, chess.engine.Limit(time=0.1))
             move = result.move
         else:
-            result = engine1.play(board, chess.engine.Limit(time=0.1))
+            result = black_engine.play(board, chess.engine.Limit(time=0.1))
             move = result.move
 
         board.push(move)
 
-    engine1.quit()
-    engine2.quit()
+    white_engine.quit()
+    black_engine.quit()
 
     return board.result()
 
+def get_inputs() -> tuple[str, int, tuple[str, str], tuple[str, str]]:
+    db_name = input("Enter database name [main.db]: ")
+    if db_name.strip() == "":
+        db_name = "main.db"
 
-# defaultdict(<class 'int'>, {'1/2-1/2': 246, '0-1': 1, '1-0': 3}) Original vs Original
+    games = input("Enter number of games to play [300]: ")
+    if games.strip() == "":
+        games = 300
+    else:
+        games = int(games)
 
-# defaultdict(<class 'int'>, {'1/2-1/2': 247, '0-1': 3}) New V original
+    white = input("Engine for white: ")
+    black = input("Engine for black: ")
 
-# defaultdict(<class 'int'>, {'1/2-1/2': 244, '0-1': 3, '1-0': 3}) Original v new
+    return db_name, games, validate_engine(white), validate_engine(black)
 
-# defaultdict(<class 'int'>, {'1/2-1/2': 243, '0-1': 3, '1-0': 4}) Original vs new2
+def validate_engine(engine: str) -> tuple[str, str]:
+    path = Path.cwd() / "engines" / engine
+    if os.path.isdir(path):
+        exes = []
+        for filename in os.listdir(path):
+            full_path = path / filename
+            if filename.lower().endswith(".exe") and os.path.isfile(full_path):
+                exes.append(full_path)
+        if len(exes) == 1:
+            return engine, exes[0]
+        else:
+            raise InvalidFileException(f"Invalid engine folder format; {len(exes)} .exe files found.")
+    else:
+        raise FileNotFoundError(f"{engine} not found.")
+
+def get_engine_model(session: Session, name: str) -> Engine:
+    stmt = select(Engine).where(Engine.name == name)
+    engine_obj = session.scalar(stmt)
+
+    if engine_obj is None:
+        engine_obj = Engine(name=name)
+        session.add(engine_obj)
+        session.commit()
+        session.refresh(engine_obj)
+
+    return engine_obj
+
+def main():
+    db_name, games, (white_name, white_engine), (black_name, black_engine) = get_inputs()
+
+    db_engine = create_engine(f"sqlite:///database/{db_name}", echo=True)
+
+    with Session(db_engine) as session:
+        white_engine_model = get_engine_model(session, white_name)
+        black_engine_model = get_engine_model(session, black_name)
+
+        tournament_model = Tournament(white_engine_id=white_engine_model.id, black_engine_id=black_engine_model.id)
+        session.add(tournament_model)
+        session.commit()
+        session.refresh(tournament_model)
+
+        # Fetch positions
+        stmt = select(ChessPosition).limit(games)
+        positions: Sequence[ChessPosition] = session.scalars(stmt).all()
+
+        for position in tqdm(positions):
+            result = play_game(position.fen, white_engine, black_engine)
+            match_model = Match(tournament_id=tournament_model.id, position_id=position.id, result=result)
+            session.add(match_model)
+            session.commit()
 
 if __name__ == "__main__":
-    games = 250
-    results = defaultdict(int)
-    with open("out.txt", "r") as file:
-        for i in tqdm(range(games)):
-            line = file.readline()
-            results[play_game(line)] += 1
-
-    print(results)
+    main()
