56d78f6e3f25c94ae8b83b2f4fbe25f518a26576
Kiril <kirbou06012@gmail.com>
2025-12-24 17:49:30 -0400

    updated training loop

--- nnue/train.py
+++ nnue/train.py
@@ -1,10 +1,27 @@
+import torch
+from torch import nn, optim
 from torch.utils.data import DataLoader
 
 from data import Positions
+from model import Model
 
 if __name__ == '__main__':
     data = Positions()
     loader = DataLoader(data)
-    for data in loader:
-        (p1, p2), value = data
-        print(data)
+
+    model = Model()
+    optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+    loss_fn = nn.MSELoss()
+    for i in range(100):
+        for (our, enemy), value in loader:
+            pred = model(our.to(torch.float32), enemy.to(torch.float32))
+
+
+            loss = loss_fn(pred, value.to(torch.float32))
+
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            print(f"Loss: {loss.item():.4f}")
