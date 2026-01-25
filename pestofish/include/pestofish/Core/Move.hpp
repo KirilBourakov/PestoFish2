38ac3c6e9d764c674f7e2a33771d70307212f822
Kiril <kirbou06012@gmail.com>
2026-01-24 21:31:25 -0400

    Removed space

--- pestofish/include/pestofish/Core/Move.hpp
+++ pestofish/include/pestofish/Core/Move.hpp
@@ -29,9 +29,9 @@ inline std::ostream& operator<<(std::ostream& os, const BoardPosition& pos) {
 }
 
 namespace Moves {
-    const uint16_t SOURCE_MASK = 0x3F;       // 0000 0000 0011 1111
-    const uint16_t TARGET_MASK = 0xFC0;      // 0000 1111 1100 0000
-    const uint16_t FLAG_MASK = 0xF000;     // 1111 0000 0000 0000
+    const uint16_t SOURCE_MASK = 0x3F; // 0000 0000 0011 1111
+    const uint16_t TARGET_MASK = 0xFC0; // 0000 1111 1100 0000
+    const uint16_t FLAG_MASK = 0xF000; // 1111 0000 0000 0000
 
     const uint16_t QUIET = 0;
     const uint16_t DOUBLE_PUSH = 1 << 12;
